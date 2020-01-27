c52c62772682d589eae059861b353652ea9a2c41
Skye <skyesladeut@outlook.com>
2020-01-27 12:15:29 -0700

    THE WHOLE THANG

---
+++ UAB_SepsisProject/ProjectSick/UABSepsim/Assets/Kyle/Scripts/Infector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Infector : MonoBehaviour
+{
+    public PatientObject MyPatient;
+    public TimeObject InGameTime;
+    public float TimeToICU = 28800;
+    public float SecondsAtCure;
+    public float Infection;
+    public float HealSubtract;
+    public float ModifiedCureSecond;
+    public bool isHealed;
+    public bool beenHealed;
+
+    void Start()
+    {
+        Infection = 0;
+    }
+    
+    void Update()
+    {
+        InfectionGrowth();
+    }
+
+    public void InfectionGrowth()
+    {
+        if (Infection >= 0)
+        {
+            if (!isHealed)
+            {
+                Infection = (100 * Mathf.Pow(10, InGameTime.SecondsPassed / TimeToICU) - 100) / 9;
+            }
+            else
+            {
+                if(!beenHealed)
+                {
+                    SecondsAtCure = InGameTime.SecondsPassed;
+                    beenHealed = true;
+                }
+
+                HealSubtract = (InGameTime.SecondsPassed - SecondsAtCure) * 2;
+                ModifiedCureSecond = InGameTime.SecondsPassed - HealSubtract;
+
+
+                Infection = (100 * Mathf.Pow(10, ModifiedCureSecond / TimeToICU) - 100) / 9;
+            }            
+        }
+    }
+}
